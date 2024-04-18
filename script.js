a395461cc717d46b8690d9d157cab430ba2dcc2a
Backa228 <andrey.scott01@gmail.com>
2024-04-18 17:33:45 +0300

    lesson18_04
    в js прописали addEventListener("mouseover", function() для элементу каталог и підключили анимацію і підключили анимацію по завершенню і при події mouseout анімація зникає

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 let items = document.querySelectorAll('.menu ul li');
 
 console.log(items);
+
+let item = document.querySelector('.menu ul li'); 
+console.log(item);
+
+item.addEventListener("mouseover", function(){
+    let catalogDiv = document.querySelector(".catalog");
+    catalogDiv.style.display = 'flex'; 
+    console.log(catalogDiv);
+
+    catalogDiv.classList.add('catalog-anim');
+
+    catalogDiv.addEventListener('animationend', function(){
+        catalogDiv.classList.remove('catalog-anim');
+        console.log(catalogDiv)
+    });
+});
+
+item.addEventListener("mouseout", function(){
+    let catalogDiv = document.querySelector(".catalog");
+    catalogDiv.style.display = 'none'; 
+    console.log(catalogDiv);
+});
