9a77d3024b1164015052e4874a3f746bfdddc219
Backa228 <andrey.scott01@gmail.com>
2024-04-25 17:33:43 +0300

    lessons19_04
    Ми підключили нову анімацію slide-out
    Створили структуру для New & Featured... створили контет для них

--- script.js
+++ script.js
@@ -1,25 +1,65 @@
 let items = document.querySelectorAll('.menu ul li');
-
 console.log(items);
 
-let item = document.querySelector('.menu ul li'); 
+let item = document.querySelector(".menu ul li");
 console.log(item);
+let isCatalogVisible = false;
+let animationInProgress = false;//Чи вже триває анімація?
 
-item.addEventListener("mouseover", function(){
-    let catalogDiv = document.querySelector(".catalog");
-    catalogDiv.style.display = 'flex'; 
+// ! - НЕ
+// || - AБО
+// && - І
+
+item.addEventListener('mouseover', function() {
+
+    if (!isCatalogVisible) {// -> isCatalogVisible == false
+
+    let catalogDiv = document.querySelector('.catalog');
+    catalogDiv.style.display = 'flex';
+    isCatalogVisible = true;//Чи елемент каталог видимий?
     console.log(catalogDiv);
 
-    catalogDiv.classList.add('catalog-anim');
+    catalogDiv.classList.add('catalog-anim-in');
+    animationInProgress = true;
+    console.log(catalogDiv);
 
-    catalogDiv.addEventListener('animationend', function(){
-        catalogDiv.classList.remove('catalog-anim');
-        console.log(catalogDiv)
+    catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
+        catalogDiv.classList.remove('catalog-anim-in');
+        animationInProgress = false;
+        console.log(catalogDiv);
+
+        catalogDiv.removeEventListener('animationend', doingAnimationEnd);
     });
+
+    
+    
+
+    
+}
+
 });
 
-item.addEventListener("mouseout", function(){
-    let catalogDiv = document.querySelector(".catalog");
-    catalogDiv.style.display = 'none'; 
+//isCatalogVisible = true;
+
+item.addEventListener('mouseout', function() {
+
+    if(isCatalogVisible) {// -> isCatalogVisible == true
+
+    let catalogDiv = document.querySelector('.catalog');
+
+    catalogDiv.classList.add('catalog-anim-out');
+    animationInProgress = true;
     console.log(catalogDiv);
+
+    catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
+        catalogDiv.classList.remove('catalog-anim-out');
+        catalogDiv.style.display = 'none';
+        isCatalogVisible = false;
+        animationInProgress = false;
+        console.log(catalogDiv);
+
+        catalogDiv.removeEventListener('animationend', doingAnimationEnd);
+    });
+}
+    
 });
