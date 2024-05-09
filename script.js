ccc7cba0e4d74db22969698f3673b5e548d8aaea
Backa228 <andrey.scott01@gmail.com>
2024-05-09 17:30:49 +0300

    lessons09.05
    оптимізували код створили 3 функції:
    showCatalog,hideCatalog,hideAndshowCatalog.Додали нові функції в цик замість минулого коду

--- script.js
+++ script.js
@@ -3,51 +3,69 @@ console.log(items);
 
 let item = document.querySelector(".menu ul li");
 console.log(item);
-let isCatalogVisible = false;
+let isCatalogVisible = false;//Чи каталог видимий
 let animationInProgress = false;//Чи вже триває анімація?
+let currentCatalogDiv = null;//буде визначати поточний каталог (той, на який курсор миші)
 
 // ! - НЕ
 // || - AБО
 // && - І
 
-item.addEventListener('mouseover', function() {
+items.forEach((itemList, index) => {
+    console.log(itemList, index);
 
-    if (!isCatalogVisible) {// -> isCatalogVisible == false
+    let catalogDiv = document.getElementById(`catalog-${index + 1}`);
 
-    let catalogDiv = document.querySelector('.catalog');
-    catalogDiv.style.display = 'flex';
-    isCatalogVisible = true;//Чи елемент каталог видимий?
-    console.log(catalogDiv);
+    itemList.addEventListener('mouseover', function () {
 
-    catalogDiv.classList.add('catalog-anim-in');
-    animationInProgress = true;
-    console.log(catalogDiv);
-
-    catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
-        catalogDiv.classList.remove('catalog-anim-in');
-        animationInProgress = false;
-        console.log(catalogDiv);
-
-        
-        catalogDiv.removeEventListener('animationend', doingAnimationEnd);
+        if (!isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == false
+            hideAndshowCatalog(catalogDiv);
+        }
+        else if (currentCatalogDiv !== catalogDiv) {
+            hideCatalog(catalogDiv)
+        }
     });
 
-    
-    
+    itemList.addEventListener('mouseout', function () {
 
-    
-}
-
+        if (!itemList.matches(":hover") && isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == true
+            hideCatalog(currentCatalogDiv);
+        }
+    });
 });
+
 
 //isCatalogVisible = true;
 
-item.addEventListener('mouseout', function() {
+function showCatalog(catalogDiv) {
+     catalogDiv.style.display = 'flex';
+            isCatalogVisible = true;//Чи елемент каталог видимий?
+            console.log(catalogDiv);
 
-    if(isCatalogVisible) {// -> isCatalogVisible == true
+            catalogDiv.classList.add('catalog-anim-in');
+            animationInProgress = true;
+            console.log(catalogDiv);
 
-    let catalogDiv = document.querySelector('.catalog');
+            catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
+                catalogDiv.classList.remove('catalog-anim-in');
+                animationInProgress = false;
+                console.log(catalogDiv);
 
+
+                catalogDiv.removeEventListener('animationend', doingAnimationEnd);
+            });
+    currentCatalogDiv = catalogDiv;
+}
+
+function hideAndshowCatalog(catalogDiv) {//catalogDiv - це параметр функції(поки щось невідоме)
+//при виклику функції currentCatalogDiv - це аргумент функції(конкретне значення)
+    if (currentCatalogDiv !== null) {
+        hideCatalog(currentCatalogDiv);
+    }
+    showCatalog(catalogDiv);
+}
+
+function hideCatalog(catalogDiv) {
     catalogDiv.classList.add('catalog-anim-out');
     animationInProgress = true;
     console.log(catalogDiv);
@@ -56,11 +74,9 @@ item.addEventListener('mouseout', function() {
         catalogDiv.classList.remove('catalog-anim-out');
         catalogDiv.style.display = 'none';
         isCatalogVisible = false;
-        animationInProgress = false;
+        animationInProgress = true;
         console.log(catalogDiv);
 
         catalogDiv.removeEventListener('animationend', doingAnimationEnd);
     });
 }
-    
-});
